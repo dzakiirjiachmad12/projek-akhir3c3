a3b3b02ade00fb93215bf62f71a0a4382c8ca16e
dzakiirjiachmad12 <ddzaki520@gmail.com>
2021-12-27 13:18:23 +0700

    four commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppFooterComponent } from './app-footer/app-footer.component';
+import { AppHeaderComponent } from './app-header/app-header.component';
+import { AppMenuComponent } from './app-menu/app-menu.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { DivisionComponent } from './division/division.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppFooterComponent,
+    AppHeaderComponent,
+    AppMenuComponent,
+    DashboardComponent,
+    DivisionComponent
   ],
   imports: [
     BrowserModule,
